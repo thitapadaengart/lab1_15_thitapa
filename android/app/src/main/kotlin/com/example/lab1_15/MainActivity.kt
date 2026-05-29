5746f3bb23913e8869c3d12ea503c20dbda960d8
thitapadaengart <thitapa.daengart@e-tecg.ac.th>
2026-05-29 13:47:44 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/lab1_15/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab1_15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+
+void main(){
+
+  String fname = "Thitapa";
+  int age = 18;
+  double money = 2.00;
+  bool x = true;
+
+
+  print("Hello $fname ! age : $age money : $money $x");
+
+
+}
